9a8f4abc8243dab29caf588949a024977baaad27
Stephanie Bucharelli <stephaniebucharelli@gmail.com>
2020-07-03 16:01:16 -0400

    Add name for function to add missing items to a new array

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-module.exports = function nameThisFunction(list) {
+module.exports = function addNewItemToArray(list) {
     var newList = []
 
-	for(var i = 0; i < list.length; i++) {
+    for (var i = 0; i < list.length; i++) {
+        // check every item in the array being passed through; check if item is not in the new list
         if (!newList.includes(list[i])) {
+            // if it's not in the newList, add the missing item
             newList.push(list[i])
         }
     }
