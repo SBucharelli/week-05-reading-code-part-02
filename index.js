a5c26be695ec9b998420fbe658697cce56ed5b2e
Stephanie Bucharelli <stephaniebucharelli@gmail.com>
2020-07-03 15:22:56 -0400

    Name highest common denominator function

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // We are using the `%` Remainder operator
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators#Remainder
 // and https://www.mathsisfun.com/definitions/modulo-operation.html
-module.exports = function function nameThisFunction(x, y) {
+module.exports = function highestCommonDenominator(x, y) {
+    // while y - as long as y is a true value
     while (y) {
         const t = y
         y = x % y
@@ -10,3 +11,17 @@ module.exports = function function nameThisFunction(x, y) {
 
     return x
 }
+
+/*
+x = 10
+y = 4
+
+const t = 4
+y = 10 % 4 ==> y = 2
+x = 4
+
+const t = 2
+y = 4 % 2 ==> y = 0
+x = 2
+return 2
+*/
